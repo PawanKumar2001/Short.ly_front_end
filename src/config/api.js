55311a33ba9c347f6b238a0b601333f4c02f092b
PawanKumar <pawan5501kumar2001@gmail.com>
2026-03-06 15:37:06 +0530

    changed base URL

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
 // API URL - Do not include trailing slash
 
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://enclosed-dude-payroll-hitting.trycloudflare.com";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://shortlyapi.dpdns.org";
